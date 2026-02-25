1a3bae9fac85655508df8048e219db4246903430
Ken Ito <amokscience@hotmail.com>
2026-02-24 18:39:33 -0600

    Update HTML title to remove hyphen in Counting app

--- main.go
+++ main.go
@@ -106,7 +106,7 @@ const htmlContent = `<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Counting - Running Table</title>
+    <title>Counting Running Table</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <style>
